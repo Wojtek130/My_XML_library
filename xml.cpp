fcc42560342411404c549f827f4d01f60a9bb530
Wojtek <pimpek86@gmail.com>
2021-06-15 14:45:31 +0200

    class hierarchy added

--- xml.cpp
+++ xml.cpp
@@ -1,1 +1,25 @@
 #include "xml.hpp"
+#include <istream>
+
+SingleTag::SingleTag(/* args */)
+{
+}
+
+SingleTag::~SingleTag()
+{
+}
+
+DoubleTag::DoubleTag(/* args */)
+{
+}
+
+DoubleTag::~DoubleTag()
+{
+}
+
+std::istream& operator>>(std::istream& is, const XMLExpression& e)
+{
+    std::string input;
+    is>>input;
+    std::cout<<input;
+}
--- xml.hpp
+++ xml.hpp
@@ -1,1 +1,45 @@
 #pragma once
+#include <unordered_map>
+#include <string>
+#include <list>
+#include <iostream>
+
+class XMLExpression
+{
+private:
+    /* data */
+public:
+    //virtual double calculate_expr() = 0;
+    //virtual std::string to_string_expr() = 0;
+    friend std::istream& operator>>(std::istream& is, const XMLExpression& e);
+    virtual ~XMLExpression() {};
+};
+
+class SingleTag : XMLExpression
+{
+protected:
+    std::unordered_map<std::string, std::string> attributes_;
+public:
+    SingleTag(/* args */);
+    ~SingleTag();
+};
+
+class DoubleTag : SingleTag
+{
+private:
+    std::list<::XMLExpression> subexpressions_;
+public:
+    DoubleTag(/* args */);
+    ~DoubleTag();
+};
+
+class Text : XMLExpression
+{
+protected:
+    std::string content_;
+public:
+    Text(/* args */);
+    ~Text();
+};
+
+
